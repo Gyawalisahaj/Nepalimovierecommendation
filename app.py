94656f815995285080d59c4c559e57fd2fc1c57b
Gyawalisahaj <sahajgnawali@gmail.com>
2026-02-24 13:47:11 +0545

    Update Streamlit frontend

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # API Configuration
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = "https://nepalimovierecommendation.vercel.app/"
 
 # Session state
 if "selected_movie" not in st.session_state:
